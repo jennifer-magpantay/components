72800f72eab697020b18cf93d7d343714ad1bc2c
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-04-16 10:09:20 +0200

    add Footer component

---
+++ file-upload/project/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Confetti } from "@phosphor-icons/react";
+
+export const Footer = () => {
+  return (
+    <footer>
+      Developed with fun by Jennifer Magpantay{" "}
+      <Confetti size={24} color="#fafafa" />
+    </footer>
+  );
+};
