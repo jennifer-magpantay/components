9556f2a1cefba28f0d6cdeb265b6283e945ab9b9
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-04-16 10:08:49 +0200

    refactor App strutcure

--- file-upload/project/src/App.jsx
+++ file-upload/project/src/App.jsx
@@ -1,9 +1,24 @@
 import "./styles/app.scss";
+import { UploadComponent } from "./components/UploadComponent";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
 export const App = () => {
   return (
-    <div>
-      <h1>Hello Stranger</h1>
-      <p>Some</p>
-    </div>
+    <>
+      <Header />
+      <div className="app-container">
+        <h1>Hello Stranger</h1>
+        <p>Some</p>
+        <main>
+          <section className="upload-files">
+            <h1>Upload Files</h1>
+            <p>Click or drag files to upload</p>
+            <UploadComponent />
+          </section>
+        </main>
+      </div>
+      <Footer />
+    </>
   );
 };
