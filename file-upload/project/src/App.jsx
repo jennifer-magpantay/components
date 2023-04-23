d7cb5699b1d9a79d38d222d880b7bd61dbdecf72
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-04-23 08:52:50 +0200

    chore Set handle for uploaded files

--- file-upload/project/src/App.jsx
+++ file-upload/project/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import "./styles/app.scss";
 
 import { Header } from "./components/Header";
@@ -5,14 +6,28 @@ import { Title } from "./components/Title";
 import { UploadComponent } from "./components/UploadComponent";
 import { Footer } from "./components/Footer";
 
+import { FilesList } from "./components/FilesList";
+
 export const App = () => {
+  const [files, setFiles] = useState([]);
+
+  const handleUploadFile = (event) => {
+    const target = event.currentTarget;
+    const inputFiles = target.files;
+    //iterate each file of FileLIst and save into state
+    Array.from(inputFiles).forEach((file) =>
+      setFiles((prev) => [...prev, file])
+    );
+  };
+
   return (
     <>
       <Header />
       <div className="app-container">
         <Title text="File Uploader" />
         <main>
-          <UploadComponent />
+          <UploadComponent onChange={(event) => handleUploadFile(event)} />
+          {files && <FilesList files={files} />}
         </main>
       </div>
       <Footer />
