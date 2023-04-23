1307757bf096da5ed6e40fe04c823a262799a5f9
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-04-23 11:27:58 +0200

    refactor Component

--- file-upload/project/src/components/UploadComponent.jsx
+++ file-upload/project/src/components/UploadComponent.jsx
@@ -2,17 +2,24 @@ import { CloudArrowUp } from "@phosphor-icons/react";
 
 export const UploadComponent = ({ onChange }) => {
   return (
-    <section className="upload-container">
-      <CloudArrowUp size={48} color="#5412c4" weight="bold" />
-      <h2 className="">Upload your files</h2>
-      <p>Click or drag files to upload</p>
-      <input
-        type="file"
-        name="file"
-        id="file-js"
-        multiple
-        onChange={onChange}
-      />
+    <section className="uploader">
+      <div className="uploader__container">
+        <CloudArrowUp
+          size={48}
+          color="#5412c4"
+          weight="bold"
+          aria-hidden={true}
+        />
+        <h2 className="">Upload your files</h2>
+        <p>Click or drag files to upload</p>
+        <input
+          id="file-js"
+          name="file"
+          type="file"
+          multiple
+          onChange={onChange}
+        />
+      </div>
     </section>
   );
 };
