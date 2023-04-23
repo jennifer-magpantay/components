f939710ae4816f4488514c063c14bb79c203c3fe
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-04-23 08:54:10 +0200

    chore Add upload file component

--- file-upload/project/src/components/UploadComponent.jsx
+++ file-upload/project/src/components/UploadComponent.jsx
@@ -1,12 +1,18 @@
 import { CloudArrowUp } from "@phosphor-icons/react";
 
-export const UploadComponent = () => {
+export const UploadComponent = ({ onChange }) => {
   return (
     <section className="upload-container">
       <CloudArrowUp size={48} color="#5412c4" weight="bold" />
       <h2 className="">Upload your files</h2>
       <p>Click or drag files to upload</p>
-      <input type="file" name="file" id="file-js" />
+      <input
+        type="file"
+        name="file"
+        id="file-js"
+        multiple
+        onChange={onChange}
+      />
     </section>
   );
 };
