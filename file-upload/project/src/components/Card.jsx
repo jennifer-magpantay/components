5c17459dd48e4a2555e94bb33bcde0e4fc834206
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-04-24 09:46:29 +0200

    Update Card.jsx

--- file-upload/project/src/components/Card.jsx
+++ file-upload/project/src/components/Card.jsx
@@ -1,20 +1,50 @@
-import { useEffect } from "react";
+import {
+  File,
+  X,
+  FileSvg,
+  FilePng,
+  ImageSquare,
+  FilePdf,
+} from "@phosphor-icons/react";
 
-import { File, X } from "@phosphor-icons/react";
+export const Card = ({ id, title, type, onClick }) => {
+  const setIcon = (type) => {
+    switch (type) {
+      case "image/svg+xml":
+        return <FileSvg size={32} />;
 
-export const Card = ({ id, title, type }) => {
-  useEffect(() => {}, []);
+      case "image/png":
+        return <FilePng size={32} />;
+
+      case "image/jpeg":
+        return <ImageSquare size={32} />;
+
+      case "application/pdf":
+        return <FilePdf size={32} />;
+
+      default:
+        return <File size={32} />;
+    }
+  };
+
+  const icon = setIcon(type);
   return (
-    <div className="card" id={id}>
+    <div className="card">
       <div className="card__aside" aria-hidden={true}>
-        <File size={32} />
+        {icon}
       </div>
       <div className="card__body">
         <p className="card__body--title">{title}</p>
         <p className="card__body--desc">{type}</p>
       </div>
       <div className="card__aside">
-        <button className="card__button--remove" type="button">
+        <button
+          className="card__button--remove"
+          id={id}
+          type="button"
+          data-id="card-button-remove"
+          onClick={onClick}
+        >
           <span className="sr-only">Remove file</span>
           <X size={32} weight="bold" />
         </button>
