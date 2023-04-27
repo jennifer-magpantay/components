9cf6eb17fdbd82f769729aff75dd0189b77758ed
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-04-27 21:55:43 +0200

    update Component style

--- file-upload/project/src/components/Card.jsx
+++ file-upload/project/src/components/Card.jsx
@@ -11,33 +11,64 @@ export const Card = ({ id, title, type, onClick }) => {
   const setIcon = (type) => {
     switch (type) {
       case "image/svg+xml":
-        return <FileSvg size={32} />;
+        return <FileSvg size={32} color="#7d4195" />;
 
       case "image/png":
-        return <FilePng size={32} />;
+        return <FilePng size={32} color="#128770" />;
 
       case "image/jpeg":
-        return <ImageSquare size={32} />;
+        return <ImageSquare size={32} color="#128770" />;
 
       case "application/pdf":
-        return <FilePdf size={32} />;
+        return <FilePdf size={32} color="#d12b1a" />;
 
       default:
-        return <File size={32} />;
+        return <File size={32} color="#3479e5" />;
     }
+  };
+
+  const setIconBackground = (type) => {
+    let color = "";
+    switch (type) {
+      case "image/svg+xml":
+        color = "#b687ca";
+        break;
+
+      case "image/png":
+        color = "#1abc9c";
+        break;
+
+      case "image/jpeg":
+        color = "#1abc9c";
+        break;
+
+      case "application/pdf":
+        color = "#e85040";
+        break;
+
+      default:
+        color = "#8eb5f1";
+        break;
+    }
+
+    return color;
   };
 
   const icon = setIcon(type);
   return (
     <div className="card">
-      <div className="card__aside" aria-hidden={true}>
+      <div
+        className="card__aside icon"
+        style={{ backgroundColor: setIconBackground(type) }}
+        aria-hidden={true}
+      >
         {icon}
       </div>
       <div className="card__body">
         <p className="card__body--title">{title}</p>
         <p className="card__body--desc">{type}</p>
       </div>
-      <div className="card__aside">
+      <div className="card__aside button">
         <button
           className="card__button--remove"
           id={id}
