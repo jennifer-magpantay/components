065db9a6c2c6bceffb69799fa7a58943ab0d095e
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-04-27 21:55:27 +0200

    update Component style and icons

--- file-upload/project/src/components/UploadComponent.jsx
+++ file-upload/project/src/components/UploadComponent.jsx
@@ -1,25 +1,35 @@
-import { CloudArrowUp } from "@phosphor-icons/react";
+import { CloudArrowUp, FolderNotchPlus } from "@phosphor-icons/react";
+import { Description } from "./Description";
 
 export const UploadComponent = ({ onChange }) => {
   return (
     <section className="uploader">
       <div className="uploader__container">
-        <CloudArrowUp
-          size={48}
-          color="#5412c4"
-          weight="bold"
-          aria-hidden={true}
-        />
-        <h2 className="section">Upload your files</h2>
-        <label htmlFor="file-js">Click or drag files to upload</label>
+        <div className="icon--container-circle" aria-hidden={true}>
+          <FolderNotchPlus
+            size={80}
+            color="#3479e5"
+            weight="fill"
+            aria-hidden={true}
+          />
+        </div>
+
+        <h2 className="section-title">
+          Drag & drop your files here or <span>upload from your computer</span>
+        </h2>
+        <label htmlFor="file-js">
+          <span className="sr-only">Click or drag files to upload</span>
+        </label>
         <input
           id="file-js"
           name="file"
           type="file"
           accept="image/png, image/jpeg, image/svg+xml, .pdf,.doc"
+          size={50}
           multiple
           onChange={onChange}
         />
+        <Description text="50 MB max file size" />
       </div>
     </section>
   );
