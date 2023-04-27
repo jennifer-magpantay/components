c15f85a00bdb6f1c157c95f93ee43679ecef901e
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-04-27 21:53:22 +0200

    update Footer content and style

--- file-upload/project/src/components/Footer.jsx
+++ file-upload/project/src/components/Footer.jsx
@@ -5,8 +5,14 @@ import { Confetti } from "@phosphor-icons/react";
 export const Footer = () => {
   return (
     <footer>
-      Developed with fun by Jennifer Magpantay{" "}
-      <Confetti size={24} color="#fafafa" />
+      <small>Developed with fun by Jennifer Magpantay </small>
+      <Confetti size={18} color="#fafafa" />{" "}
+      <small>
+        Perfectly designed by{" "}
+        <a href="https://dribbble.com/kobmax" target="_blank">
+          Maksym Kobets
+        </a>
+      </small>
     </footer>
   );
 };
