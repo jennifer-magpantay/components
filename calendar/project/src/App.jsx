87cc0cc565e4c2f79e161ded0f8cc7f39d19ac69
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-05-06 11:20:38 +0200

    style Set project style

--- calendar/project/src/App.jsx
+++ calendar/project/src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./styles/app.scss";
+
+import { Header } from "../../../file-upload/project/src/components/Header";
+import { Footer } from "../../../file-upload/project/src/components/Footer";
+import { Title } from "../../../file-upload/project/src/components/Title";
 
 export const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Header />
+      <main>
+        <Title text="Calendar" />
+      </main>
+      <Footer />
+    </>
+  );
+};
