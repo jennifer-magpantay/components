297cc7340d8a1c1b7f5fea4b128a2dd33b28cc3a
Jennifer Magpantay <56962997+jennifer-magpantay@users.noreply.github.com>
2023-05-06 14:07:12 +0200

    chore Add description component

--- calendar/project/src/App.jsx
+++ calendar/project/src/App.jsx
@@ -4,6 +4,7 @@ import "./styles/app.scss";
 import { Header } from "../../../file-upload/project/src/components/Header";
 import { Footer } from "../../../file-upload/project/src/components/Footer";
 import { Title } from "../../../file-upload/project/src/components/Title";
+import { Description } from "./components/Description";
 
 export const App = () => {
   return (
@@ -11,6 +12,18 @@ export const App = () => {
       <Header />
       <main>
         <Title text="Calendar" />
+        <Description text="Simple date picker built with react-calendar to add to any project" />
+        <div className="calendar--container" data-calendar>
+          <input
+            type="text"
+            className="calendar"
+            id="calendar-js"
+            name="calendar"
+            placeholder="Select a date from the calendar"
+            readOnly
+          />
+          <label htmlFor="calendar-js">Choose date</label>
+        </div>
       </main>
       <Footer />
     </>
